441c20ac85d745da36778850758d4c9e53092752
Johnkhk <khh019@ucsd.edu>
2021-09-24 19:17:01 -0700

    favicon added

--- gh-pages.js
+++ gh-pages.js
@@ -5,7 +5,7 @@ ghpages.publish(
 	{
 		branch: 'gh-pages',
 		// repo: 'https://github.com/el3um4s/petits-chevaux.git', // Update to point to your repository
-		repo: 'https://github.com/Johnkhk/johnho.github.io.git', // Update to point to your repository
+		repo: 'https://github.com/Johnkhk/portfolio.git', // Update to point to your repository
 		user: {
 			name: 'Johnkhh', // update to use your name
 			email: 'khh019@ucsd.edu' // Update to use your email
