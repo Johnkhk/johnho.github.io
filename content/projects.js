df77392674c4699a3f699e41f235835be29ef81c
Johnkhk <khh019@ucsd.edu>
2021-12-02 04:36:51 -0800

    added polar coffee

--- content/projects.js
+++ content/projects.js
@@ -40,6 +40,21 @@ export const projects =
          }
       },
       {
+         "titulo": "Polar Coffee",
+         "src": "./images/polarcoffland.PNG",
+         "src_dsk": "./images/polarcollage.jpg",
+         "contenido": "Esta es una extensión de VSCode que creé para mejorar mi flujo de trabajo con Python. \
+          Permite al usuario ejecutar comandos comunes como tareas de VSCode en vez de escribirlos manualmente cada vez. \
+          Aunque la creé principalmente para mi uso personal, tiene más de 5000 descargas en VSCode Marketplace",
+
+         "contenido_en": "Polar Coffee is a startup idea for an IoT Iced Coffee Machine. Through a 2 part course, me and my team built this product and won best overall in the class through diligent application of full-stack engineering, market research, and methods related to the realistic development of a commercial product. We also presented the product as a <a href='https://drive.google.com/file/d/1zZLrmWez2hXWmzLHEY1FbWygJMwgkBMx/view?usp=sharing'>startup presentation pitch</a>. And developed mock patents trademarks, as well as showcased a live <a href='https://drive.google.com/file/d/1lS2Jx9CY7lQg675WZyDoCoT_4ZyNhevi/view?usp=sharing'>demo</a> of the MVP. \
+         ",
+         "liveUrl": {
+            "desktop": "https://drive.google.com/file/d/1lS2Jx9CY7lQg675WZyDoCoT_4ZyNhevi/view?usp=sharing",
+            "mobile": "https://drive.google.com/file/d/1lS2Jx9CY7lQg675WZyDoCoT_4ZyNhevi/view?usp=sharing"
+         }
+      },
+      {
          "titulo": "React SVG arrows on Media Player",
          "src": "./images/svgdraw.png",
          "src_dsk": "./images/svgdraw.png",
