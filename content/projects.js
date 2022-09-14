69a363fefcfa925228a47ecf46dfc27c4bcde57b
johnkhk <khh019@ucsd.edu>
2022-09-14 22:22:30 +0800

    main is useless

--- content/projects.js
+++ content/projects.js
@@ -1,7 +1,7 @@
 export const projects =
    [
       {
-         "titulo":"Cryptotrackerr.tk",
+         "titulo":"Cryptotracker.fyi",
          // "titulo": "bruh",
 
          // "src": "./images/blog_mobile.png",
@@ -14,12 +14,12 @@ export const projects =
           La idea principal detrás de este proyecto fue practicar el uso de ciertas tecnologías y los detalles de implementación de ciertas funcionalidades.<br><br> \
            El código está disponible públicamente así puedo recibir retroalimentación y crear una referencia para aquellos que quieran implementar algo similar. ",
 
-         "contenido_en": "This <a href='https://github.com/Johnkhk/CryptoTrackerWeb2.0'>website</a> lets users sign in and track crypto currencies using the CoinMarketCap API. \
+         "contenido_en": "This <a href='https://github.com/Johnkhk/CryptoTrackerWeb3.0'>website</a> lets users sign in and track cryptocurrencies using the CoinMarketCap API. \
           It was built with React, Node, MongoDB, and hosted on an Amazon EC2 instance. \
           ",
          "liveUrl": {
-            "desktop": "http://cryptotrackerr.tk",
-            "mobile": "http://cryptotrackerr.tk"
+            "desktop": "http://cryptotracker.fyi",
+            "mobile": "http://cryptotracker.fyi"
          }
       },
       {
@@ -37,6 +37,21 @@ export const projects =
          "liveUrl": {
             "desktop": "https://drive.google.com/file/d/1Ept2hMXhE1bvbfPlltUMjSQm5NPYN5Up/view?usp=sharing",
             "mobile": "https://drive.google.com/file/d/1Ept2hMXhE1bvbfPlltUMjSQm5NPYN5Up/view?usp=sharing"
+         }
+      },
+      {
+         "titulo": "Polar Coffee",
+         "src": "./images/polarcoffland.PNG",
+         "src_dsk": "./images/polarcollage.jpg",
+         "contenido": "Esta es una extensión de VSCode que creé para mejorar mi flujo de trabajo con Python. \
+          Permite al usuario ejecutar comandos comunes como tareas de VSCode en vez de escribirlos manualmente cada vez. \
+          Aunque la creé principalmente para mi uso personal, tiene más de 5000 descargas en VSCode Marketplace",
+
+         "contenido_en": "Polar Coffee is a startup idea for an IoT Iced Coffee Machine. Through a 2 part course, me and my team built this product and won best overall in the class through diligent application of full-stack engineering, market research, and methods related to the realistic development of a commercial product. We also presented the product as a <a href='https://drive.google.com/file/d/1zZLrmWez2hXWmzLHEY1FbWygJMwgkBMx/view?usp=sharing'>startup presentation pitch</a>. And developed mock patents trademarks, as well as showcased a live <a href='https://drive.google.com/file/d/1lS2Jx9CY7lQg675WZyDoCoT_4ZyNhevi/view?usp=sharing'>demo</a> of the MVP. \
+         ",
+         "liveUrl": {
+            "desktop": "https://drive.google.com/file/d/1lS2Jx9CY7lQg675WZyDoCoT_4ZyNhevi/view?usp=sharing",
+            "mobile": "https://drive.google.com/file/d/1lS2Jx9CY7lQg675WZyDoCoT_4ZyNhevi/view?usp=sharing"
          }
       },
       {
